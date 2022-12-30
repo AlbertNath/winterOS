43f6c3a228066b5a8ff25d6bae5f0e6be90fa508
Alberto Natanael Medel Piña <natanael_pi@ciencias.unam.mx>
2022-12-29 21:12:51 -0600

    Post 2 - Building our Kernel

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-#![no_std]
-#![no_main]
+#![no_std] // don't link with standar  library
+#![no_main] // disable Rust-level entry points
 
 use core::panic::PanicInfo;
 
-#[no_mangle]
+#[no_mangle] // don't mangle this function (it's the entry point)
 pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+/// This is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
