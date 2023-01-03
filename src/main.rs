76fbe4af0bbccfacf2ceffb991497af16b566215
Alberto Natanael Medel Piña <natanael_pi@ciencias.unam.mx>
2023-01-02 21:09:04 -0600

    Post 3 - Success Exit Code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 #![no_std] // don't link with standar  library
 #![no_main] // disable Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
 use core::panic::PanicInfo;
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed  = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
 
 static HELLO: &[u8] = b"Hello world!";
 
@@ -11,6 +29,8 @@ static HELLO: &[u8] = b"Hello world!";
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     // panic!("Some panic message");
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
@@ -21,4 +41,21 @@ fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
 
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("A trivial assertion... ");
+    assert_eq!(1,1);
+    println!("[ok]");
 }
