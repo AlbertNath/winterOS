f480f1b2f4380b47bc09fbc781c24f13e2a0d05d
Alberto Natanael Medel Piña <natanael_pi@ciencias.unam.mx>
2023-01-07 15:26:14 -0600

    Post 3 - Timeouts (Testable trait implemented)

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,21 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) -> () {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -54,18 +69,15 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
-
     exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("A trivial assertion... ");
-    assert_eq!(0,1);
-    serial_println!("[ok]");
+    assert_eq!(1,1);
 }
