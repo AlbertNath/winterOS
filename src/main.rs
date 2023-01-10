439e075b590413806c14a6fb3d5183ca3ce7bda6
Alberto Natanael Medel Piña <natanael_pi@ciencias.unam.mx>
2023-01-09 18:38:41 -0600

    Post 3 - Future tests
    This reverts commit 330f3e01f92c868832d8dcd94d675821d4465fb2.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,16 @@
 #![no_std] // don't link with standar  library
 #![no_main] // disable Rust-level entry points
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(winter_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-mod serial;
-
 use core::panic::PanicInfo;
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed  = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-static HELLO: &[u8] = b"Hello world!";
+use winter_os::println;
 
 #[no_mangle] // don't mangle this function (it's the entry point)
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    // panic!("Some panic message");
+
     #[cfg(test)]
     test_main();
 
@@ -44,40 +25,8 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) -> () {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1,1);
+    winter_os::test_panic_handler(info)
 }
