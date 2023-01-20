fd09ab3ef5deea2a1a2a3b89d5c060915106b4da
Juan Pablo Yamamoto <jpyamamoto@ciencias.unam.mx>
2023-01-20 12:32:17 -0600

    CPU exceptions

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,11 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+#![feature(abi_x86_interrupt)]
+
 pub mod vga_buffer;
 pub mod serial;
+pub mod interrupts;
 
 use core::panic::PanicInfo;
 
@@ -62,6 +65,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
@@ -70,4 +74,8 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     test_panic_handler(info)
+}
+
+pub fn init() {
+    interrupts::init_idt();
 }
--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,14 @@ use winter_os::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    winter_os::init();
+
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("Didn't crash c:");
     loop {}
 }
 
